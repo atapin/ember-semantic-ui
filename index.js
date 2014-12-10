eba74cc5a2861863cd62c2d20a75ddea2ab29740
Josh Forisha <josh@forisha.com>
2014-12-09 21:28:49 -0600

    Import Semantic UI's fonts and image

--- index.js
+++ index.js
@@ -4,7 +4,15 @@
 module.exports = {
   name: 'ember-semantic-ui',
 
-  included: function ( app ) {
+  included: function (app) {
+    app.import('bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.eot');
+    app.import('bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.otf');
+    app.import('bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.svg');
+    app.import('bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.ttf');
+    app.import('bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.woff');
+
+    app.import('bower_components/semantic-ui/dist/themes/default/assets/images/flags.png');
+
     app.import({
       development: 'bower_components/semantic-ui/dist/semantic.css',
       production: 'bower_components/semantic-ui/dist/semantic.min.css'
