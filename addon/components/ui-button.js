1911c8265839af65f6914db7fe7b6b529af59f2f
Josh Forisha <josh@forisha.com>
2014-12-09 20:32:06 -0600

    Add ui-button component

---
+++ addon/components/ui-button.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  classNameBindings: [ 'loading' ],
+
+  classNames: [ 'ui', 'button' ]
+
+});
---
+++ app/components/ui-button.js
@@ -0,0 +1,3 @@
+import UiButton from 'ember-semantic-ui/components/ui-button';
+
+export default UiButton;
